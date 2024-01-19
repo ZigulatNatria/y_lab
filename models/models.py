56ba03ce189519f03c802faf6678854fedac9d4d
ZigulatNatria <vachrameev.oleg@yandex.ru>
2024-01-20 00:34:49 +0500

    Initial

---
+++ models/models.py
@@ -0,0 +1,34 @@
+from sqlalchemy import MetaData, Integer, ForeignKey, Table, Column, String, Float
+
+
+metadata = MetaData()
+
+
+menu = Table(
+    "menu",
+    metadata,
+    Column('id', Integer, primary_key=True),
+    Column('title', String, nullable=False),
+    Column('description', String, nullable=False),
+)
+
+
+submenu = Table(
+    "submenu",
+    metadata,
+    Column('id', Integer, primary_key=True),
+    Column('title', String, nullable=False),
+    Column('description', String, nullable=False),
+    Column('submenu', Integer, ForeignKey("menu.id"))
+)
+
+
+dish = Table(
+    "dish",
+    metadata,
+    Column('id', Integer, primary_key=True),
+    Column('title', String, nullable=False),
+    Column('description', String, nullable=False),
+    Column('price', Float, nullable=False),
+    Column('submenu', Integer, ForeignKey("submenu.id"))
+)
